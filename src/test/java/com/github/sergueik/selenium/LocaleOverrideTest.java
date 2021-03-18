2fd4178c84156d261e0287781681e0aa5b92f780
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2021-03-17 23:50:16 -0400

    intermediate revision - added "Emulation.setLocaleOverride" test, and the call does not seem to cause the effect

---
+++ src/test/java/com/github/sergueik/selenium/LocaleOverrideTest.java
@@ -0,0 +1,52 @@
+package com.github.sergueik.selenium;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.Test;
+
+/**
+ * Selected test scenarios for Selenium 4 Chrome Developer Tools bridge inspired
+ *
+ * @author: Serguei Kouzmine (kouzmine_serguei@yahoo.com)
+ */
+public class LocaleOverrideTest extends BaseCdpTest {
+
+	private static String command = null;
+	private static Map<String, Object> result = null;
+	private static Map<String, Object> data = null;
+	private static Map<String, Object> params = new HashMap<>();
+	private static final String baseURL = "https://www.wikipedia.org";
+
+	@SuppressWarnings("unchecked")
+	@Test
+	// https://phrase.com/blog/posts/detecting-a-users-locale/
+	public void test3() {
+		// Arrange
+		command = "Emulation.setLocaleOverride";
+		// Act
+		params = new HashMap<>();
+		params.put("locale", "ru-RU");
+		driver.executeCdpCommand(command, params);
+		driver.executeCdpCommand("Runtime.enable", new HashMap<>());
+
+		driver.get(baseURL);
+		command = "Runtime.evaluate";
+		params = new HashMap<>();
+		params.put("expression",
+				"function example(){ return navigator.language;} example();");
+		result = driver.executeCdpCommand(command, params);
+		System.err.println("Response to " + command + ": " + result);
+		command = "Runtime.evaluate";
+		params = new HashMap<>();
+		params.put("expression",
+				"function example(){ return navigator.languages;} example();");
+		result = driver.executeCdpCommand(command, params);
+		System.err.println("Response to " + command + ": " + result);
+		command = "Runtime.getProperties";
+		params = new HashMap<>();
+		data = (Map<String, Object>) result.get("result");
+		params.put("objectId", data.get("objectId").toString());
+		result = driver.executeCdpCommand(command, params);
+		System.err.println("Response to " + command + ": " + result);
+	}
+}
