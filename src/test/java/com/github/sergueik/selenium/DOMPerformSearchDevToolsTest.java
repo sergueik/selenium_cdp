ad95c920b591327784f65aa34daf5d52131a64f6
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2023-01-14 04:00:09 +0100

    intermediate revision - added "DOM.performSearch"/ "DOM.getSearchResults" test  - failing to fill the resuslt NodeIds

---
+++ src/test/java/com/github/sergueik/selenium/DOMPerformSearchDevToolsTest.java
@@ -0,0 +1,99 @@
+package com.github.sergueik.selenium;
+
+/**
+ * Copyright 2023 Serguei Kouzmine
+ */
+
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.hasKey;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.openqa.selenium.WebDriverException;
+
+import com.google.gson.JsonSyntaxException;
+
+import org.openqa.selenium.devtools.DevToolsException;
+import org.openqa.selenium.devtools.v108.dom.DOM;
+import org.openqa.selenium.devtools.v108.dom.DOM.EnableIncludeWhitespace;
+import org.openqa.selenium.devtools.v108.dom.DOM.PerformSearchResponse;
+import org.openqa.selenium.devtools.v108.dom.model.Node;
+import org.openqa.selenium.devtools.v108.dom.model.NodeId;
+import org.openqa.selenium.devtools.v108.dom.model.SetChildNodes;
+
+/**
+ * Selected test scenarios for Selenium 4 Chrome Developer Tools bridge
+ * https://chromedevtools.github.io/devtools-protocol/tot/DOM/#method-getDocument
+ * https://chromedevtools.github.io/devtools-protocol/tot/DOM/#method-querySelectorAll
+ * https://chromedevtools.github.io/devtools-protocol/tot/DOM/#method-performSearch
+ * https://chromedevtools.github.io/devtools-protocol/tot/DOM/#type-Node
+ * https://chromedevtools.github.io/devtools-protocol/1-3/DOM/#event-setChildNodes
+ * @author: Serguei Kouzmine (kouzmine_serguei@yahoo.com)
+ */
+
+public class DOMPerformSearchDevToolsTest extends BaseDevToolsTest {
+
+	private static String baseURL = "https://www.wikipedia.org";
+	private final String selector = "a.other-project-link";
+
+	@After
+	public void afterTest() {
+		chromeDevTools.send(DOM.disable());
+		driver.get("about:blank");
+	}
+
+	@Before
+	public void beforeTest() {
+		EnableIncludeWhitespace enableIncludeWhitespace = EnableIncludeWhitespace.ALL;
+		driver.get(baseURL);
+		chromeDevTools.send(DOM.enable(Optional.of(enableIncludeWhitespace)));
+	}
+
+	@Test
+	public void test1() {
+		try {
+			Node result = chromeDevTools
+					.send(DOM.getDocument(Optional.of(1), Optional.of(true)));
+
+			List<NodeId> results = chromeDevTools.send(
+					DOM.querySelectorAll(result.getNodeId(), "a.other-project-link"));
+			System.err.println(String.format("Found %d Node Ids:", results.size()));
+			results.forEach(nodeId -> System.err
+					.println(String.format("Id: %s\nHTML: %s", nodeId.toString(),
+							chromeDevTools.send(DOM.getOuterHTML(Optional.of(nodeId),
+									Optional.empty(), Optional.empty())))));
+		} catch (DevToolsException e) {
+			System.err.println("Exception (rethrown) " + e.getMessage());
+			throw e;
+		}
+	}
+
+	// NOTE: "DOM.getSearchResults" is returning array of zero NodeIds
+	@Test
+	public void test2() {
+		try {
+			PerformSearchResponse searchResponse = chromeDevTools
+					.send(DOM.performSearch(selector, Optional.of(true)));
+			Utils.sleep(1000); // no effect
+			System.err.println(
+					String.format("returned %d nodes", searchResponse.getResultCount()));
+			List<NodeId> results = chromeDevTools.send(DOM.getSearchResults(
+					searchResponse.getSearchId(), 0, searchResponse.getResultCount()));
+			System.err.println(String.format("Node Ids: %s", results.toString()));
+		} catch (DevToolsException e) {
+			System.err.println("Exception (rethrown) " + e.getMessage());
+			throw e;
+		}
+	}
+}
