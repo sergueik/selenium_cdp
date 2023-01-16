26a8e3de5248570f22f121a9d715aa92a5a6a77f
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2023-01-17 00:40:28 +0100

    intermediate revision - added attempt to exercise "Fetch.enable" and  "Fetch.requestPaused" but no luck with incorrect RequestPattern

---
+++ src/test/java/com/github/sergueik/selenium/FetcFailRequestDevToolsTest.java
@@ -0,0 +1,109 @@
+package com.github.sergueik.selenium;
+/**
+ * Copyright 2023 Serguei Kouzmine
+ */
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.TimeoutException;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import org.openqa.selenium.devtools.v108.fetch.Fetch;
+import org.openqa.selenium.devtools.v108.fetch.model.RequestId;
+import org.openqa.selenium.devtools.v108.fetch.model.RequestPattern;
+import org.openqa.selenium.devtools.v108.fetch.model.RequestPaused;
+import org.openqa.selenium.devtools.v108.network.Network;
+import org.openqa.selenium.devtools.v108.network.model.ErrorReason;
+import org.openqa.selenium.devtools.v108.network.model.Request;
+import org.openqa.selenium.remote.UnreachableBrowserException;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+/**
+ * Selected test scenarios for Selenium Chrome Developer Tools Selenium 4 bridge
+ * https://chromedevtools.github.io/devtools-protocol/tot/Fetch/
+ * https://chromedevtools.github.io/devtools-protocol/tot/Fetch/#method-failRequest
+ * https://www.selenium.dev/selenium/docs/api/java/org/openqa/selenium/devtools/NetworkInterceptor.html
+ * https://www.selenium.dev/selenium/docs/api/java/org/openqa/selenium/remote/http/Route.html
+ * https://www.selenium.dev/selenium/docs/api/java/org/openqa/selenium/remote/http/HttpRequest.html
+ * @author: Serguei Kouzmine (kouzmine_serguei@yahoo.com)
+ */
+
+public class FetcFailRequestDevToolsTest extends BaseDevToolsTest {
+	private static String url = "https://www.wikipedia.org";
+	private static WebDriverWait wait;
+	private static int flexibleWait = 10;
+	private static int pollingInterval = 500;
+	private static WebElement element = null;
+
+	@Before
+	public void before() throws TimeoutException {
+		String urlPattern = ".*/portal/wikipedia.org/assets/.*";
+
+		RequestPattern requestPattern = new RequestPattern(Optional.of(urlPattern),
+				Optional.empty(), Optional.empty());
+		List<RequestPattern> arg = new ArrayList<>();
+		arg.add(requestPattern);
+		chromeDevTools.send(Fetch.enable(Optional.of(arg) /* Optional.empty()*/,
+				Optional.of(false)));
+		chromeDevTools.send(Network.clearBrowserCache());
+		chromeDevTools.send(Network.setCacheDisabled(true));
+
+		chromeDevTools.addListener(Fetch.requestPaused(), (RequestPaused event) -> {
+			Request request = event.getRequest();
+			RequestId requestId = event.getRequestId();
+			System.err.println("About to handle the request: " + request.getUrl());
+			if (request.getUrl().matches(".*\\.(?:png|jpg|jpeg)$")) {
+				System.err.println("About to abort the request to " + request.getUrl());
+				ErrorReason errorReason = ErrorReason.ABORTED;
+				Fetch.failRequest(requestId, errorReason);
+			} else {
+				// NOTE: setting HTTP response code by hand
+				Fetch.continueRequest(requestId, Optional.of(request.getUrl()),
+						Optional.of(request.getMethod()), request.getPostData(),
+						/* Optional.of(request.getHeaders()) */ Optional.empty(),
+						Optional.of(false));
+			}
+		});
+
+	}
+
+	@Test
+	public void test2() {
+		driver.navigate().to("https://www.wikipedia.org");
+
+		wait = new WebDriverWait(driver, Duration.ofSeconds(flexibleWait));
+
+		wait.pollingEvery(Duration.ofMillis(pollingInterval));
+		element = wait.until(ExpectedConditions.visibilityOfElementLocated(
+				By.cssSelector("img.central-featured-logo")));
+
+		Long naturalWidth = (Long) driver
+				.executeScript("return arguments[0].naturalWidth", element);
+		Long naturalHeight = (Long) driver
+				.executeScript("return arguments[0].naturalHeight", element);
+		// assertThat(naturalWidth, is(0L));
+		// assertThat(naturalHeight, is(0L));
+	}
+
+	@After
+	public void after() {
+
+		chromeDevTools.send(Network.setBlockedURLs(new ArrayList<String>()));
+		chromeDevTools.send(Network.disable());
+		chromeDevTools.send(Network.setCacheDisabled(false));
+		chromeDevTools.clearListeners();
+	}
+
+}
