f561ef974bdc5f2290c783bcbb33b3ba7dfc9419
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2023-01-16 02:16:53 +0100

    intermediate revision - added CDP and DevTools commands to Ignore Site Certificate Errors

---
+++ src/test/java/com/github/sergueik/selenium/IgnoreCertificateErrorsCdpTest.java
@@ -0,0 +1,126 @@
+package com.github.sergueik.selenium;
+
+/**
+ * Copyright 2023 Serguei Kouzmine
+ */
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasKey;
+import static org.junit.Assert.assertTrue;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import com.google.gson.JsonSyntaxException;
+
+/**
+ * Selected test scenarios for Selenium 4 Chrome Developer Tools bridge
+ * https://chromedevtools.github.io/devtools-protocol/tot/Security/#method-setIgnoreCertificateErrors
+ * https://chromedevtools.github.io/devtools-protocol/tot/DOM/#method-getOuterHTML
+ * @author: Serguei Kouzmine (kouzmine_serguei@yahoo.com)
+ */
+
+// based on:
+// https://github.com/shsarkar08/PythonSeleniumCDP_APIs/blob/master/sel_py_cdp_ignore_cert_errors.py
+public class IgnoreCertificateErrorsCdpTest extends BaseCdpTest {
+
+	private static String baseURL = "https://untrusted-root.badssl.com/";
+	private final String selector = "#footer";
+
+	private static String command = "Security.setIgnoreCertificateErrors";
+
+	private static Map<String, Object> params = new HashMap<>();
+	private static Map<String, Object> result = new HashMap<>();
+	public static Long nodeId = (long) -1;
+	private static WebElement element = null;
+
+	@After
+	public void afterTest() {
+		command = "Security.setIgnoreCertificateErrors";
+		driver.get("about:blank");
+		params.clear();
+		params.put("ignore", false);
+		result = driver.executeCdpCommand(command, params);
+	}
+
+	@Before
+	public void beforeTest() {
+		command = "Security.setIgnoreCertificateErrors";
+		params.clear();
+		params.put("ignore", true);
+		result = driver.executeCdpCommand(command, params);
+		driver.get(baseURL);
+	}
+
+	@Ignore
+	@Test
+	public void test1() {
+		element = wait.until(ExpectedConditions
+				.visibilityOfElementLocated(By.cssSelector(selector)));
+		String data = element.getText();
+		assertThat(data, containsString(
+				"The certificate for this site is signed using an untrusted root."));
+		System.err.println(data);
+
+	}
+
+	// @Ignore("appears broken in DOM navigation")
+	@SuppressWarnings("unchecked")
+	@Test
+	public void test2() {
+
+		try {
+			command = "DOM.getDocument";
+			params = new HashMap<>();
+			params.put("pierce", false);
+			params.put("depth", 1);
+			// Act
+			result = driver.executeCdpCommand(command, params);
+			nodeId = Long.parseLong(
+					((Map<String, Object>) result.get("root")).get("nodeId").toString());
+			System.err.println(String.format("nodeid: %d", nodeId));
+			command = "DOM.querySelector";
+			params.clear();
+
+			params.put("nodeId", nodeId);
+			params.put("selector", selector);
+
+			result = driver.executeCdpCommand(command, params);
+
+			assertThat(result, hasKey("nodeId"));
+			nodeId = (Long) result.get("nodeId");
+			System.err.println(String.format("nodeid: %d", nodeId));
+			command = "DOM.getOuterHTML";
+			params.clear();
+			params.put("nodeId", nodeId);
+			result = driver.executeCdpCommand(command, params);
+			assertThat(result, notNullValue());
+			assertTrue(result.containsKey("outerHTML"));
+			String data = result.get("outerHTML").toString();
+			assertThat(data, containsString(
+					"The certificate for this site is signed using an untrusted root."));
+			System.err.println(data);
+		} catch (JsonSyntaxException e) {
+			System.err.println("JSON Syntax exception in " + command + " (ignored): "
+					+ e.toString());
+		} catch (WebDriverException e) {
+			System.err.println("Web Driver exception in " + command + " (ignored): "
+					+ Utils.processExceptionMessage(e.getMessage()));
+		} catch (Exception e) {
+			System.err.println("Exception in " + command + "  " + e.toString());
+			e.printStackTrace();
+			throw (new RuntimeException(e));
+		}
+	}
+}
---
+++ src/test/java/com/github/sergueik/selenium/IgnoreCertificateErrorsDevToolsTest.java
@@ -0,0 +1,102 @@
+package com.github.sergueik.selenium;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.hasKey;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.devtools.DevToolsException;
+import org.openqa.selenium.devtools.v108.dom.DOM;
+import org.openqa.selenium.devtools.v108.dom.DOM.EnableIncludeWhitespace;
+import org.openqa.selenium.devtools.v108.dom.model.Node;
+import org.openqa.selenium.devtools.v108.dom.model.NodeId;
+import org.openqa.selenium.devtools.v108.security.Security;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+/**
+ * Selected test scenarios for Selenium 4 Chrome Developer Tools bridge
+ * https://chromedevtools.github.io/devtools-protocol/tot/Security/#method-setIgnoreCertificateErrors
+ * https://chromedevtools.github.io/devtools-protocol/tot/Security/#event-certificateError
+ * @author: Serguei Kouzmine (kouzmine_serguei@yahoo.com)
+ */
+
+// based on:
+// https://github.com/shsarkar08/PythonSeleniumCDP_APIs/blob/master/sel_py_cdp_ignore_cert_errors.py
+
+public class IgnoreCertificateErrorsDevToolsTest extends BaseDevToolsTest {
+
+	private static String baseURL = "https://untrusted-root.badssl.com/";
+	private final String selector = "#footer";
+
+	private static Node result;
+	private static NodeId nodeId = null;
+	private static WebDriverWait wait;
+	private static int flexibleWait = 10;
+	private static int pollingInterval = 500;
+
+	private static WebElement element = null;
+
+	@After
+	public void afterTest() {
+		chromeDevTools.send(Security.setIgnoreCertificateErrors(false));
+		chromeDevTools.send(DOM.disable());
+		driver.get("about:blank");
+	}
+
+	@Before
+	public void beforeTest() {
+		chromeDevTools.send(Security.setIgnoreCertificateErrors(true));
+		driver.get(baseURL);
+		EnableIncludeWhitespace enableIncludeWhitespace = EnableIncludeWhitespace.ALL;
+
+		chromeDevTools.send(DOM.enable(Optional.of(enableIncludeWhitespace)));
+	}
+
+	@Test
+	public void test1() {
+		wait = new WebDriverWait(driver, Duration.ofSeconds(flexibleWait));
+
+		wait.pollingEvery(Duration.ofMillis(pollingInterval));
+		element = wait.until(ExpectedConditions
+				.visibilityOfElementLocated(By.cssSelector(selector)));
+		String data = element.getText();
+		assertThat(data, containsString(
+				"The certificate for this site is signed using an untrusted root."));
+		System.err.println(data);
+	}
+
+	@Test
+	public void test2() {
+		try {
+			result = chromeDevTools
+					.send(DOM.getDocument(Optional.of(1), Optional.of(true)));
+
+			nodeId = chromeDevTools
+					.send(DOM.querySelector(result.getNodeId(), selector));
+			String data = chromeDevTools.send(DOM.getOuterHTML(Optional.of(nodeId),
+					Optional.empty(), Optional.empty()));
+			assertThat(data, containsString(
+					"The certificate for this site is signed using an untrusted root."));
+
+			System.err.println(data);
+
+		} catch (DevToolsException e) {
+			System.err.println("Exception (rethrown) " + e.getMessage());
+			throw e;
+		}
+	}
+
+}
