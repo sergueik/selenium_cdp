99e16ef6a731955aa8a66499e575f8883774bb70
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2023-02-07 03:18:16 +0100

    intermediate revision - added "Page.screencastFrameAck" and receives full setof frames

--- src/test/java/com/github/sergueik/selenium/ScreenCastFramesDevToolsTest.java
+++ src/test/java/com/github/sergueik/selenium/ScreenCastFramesDevToolsTest.java
@@ -4,8 +4,6 @@ package com.github.sergueik.selenium;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
 import java.util.Optional;
 
 import org.apache.commons.codec.binary.Base64;
@@ -27,8 +25,9 @@ import org.openqa.selenium.devtools.v109.page.model.ScreencastFrameMetadata;
  * https://chromedevtools.github.io/devtools-protocol/tot/Page/#event-screencastFrame
  * https://chromedevtools.github.io/devtools-protocol/tot/Page/#method-startScreencast
  * https://chromedevtools.github.io/devtools-protocol/tot/Page/#method-reload
+ * https://chromedevtools.github.io/devtools-protocol/tot/Page/#method-screencastFrameAck
  * https://javadoc.io/doc/org.seleniumhq.selenium/selenium-devtools-v109/latest/overview-summary.html
- * 
+ *  
  * @author: Serguei Kouzmine (kouzmine_serguei@yahoo.com)
  */
 
@@ -38,9 +37,14 @@ public class ScreenCastFramesDevToolsTest extends BaseDevToolsTest {
 	private final int maxHeight = 480;
 	private final int quality = 100;
 	private final int everyNthFrame = 10;
-	private final boolean save = true;
 	private final StartScreencastFormat format = Page.StartScreencastFormat.PNG;
 	private String screencastFileName = null;
+	private static long nowEpoch;
+	private static long frameEpoch;
+	private static String data;
+	private static boolean debug = true;
+	private static byte[] image;
+	private static Integer sesionId;
 	private final static Base64 base64 = new Base64();
 
 	@Before
@@ -65,38 +69,39 @@ public class ScreenCastFramesDevToolsTest extends BaseDevToolsTest {
 	@Test
 	public void test() {
 		// Arrange
-
-		long nowEpoch = System.currentTimeMillis() / 1000;
+		// add to the seconds since the start of the recorging to the screenshot
+		// filename
+		nowEpoch = System.currentTimeMillis() / 1000;
 		chromeDevTools.addListener(Page.screencastFrame(),
 				(ScreencastFrame screencastFrame) -> {
 
 					try {
 						ScreencastFrameMetadata metadata = screencastFrame.getMetadata();
-						Number deviceHeight = metadata.getDeviceHeight();
-						Number scaleFactor = metadata.getPageScaleFactor();
-						Number deviceWidth = metadata.getDeviceWidth();
+						// Number deviceHeight = metadata.getDeviceHeight();
+						// Number scaleFactor = metadata.getPageScaleFactor();
+						// Number deviceWidth = metadata.getDeviceWidth();
 						TimeSinceEpoch timestamp = metadata.getTimestamp().get();
 
 						String data = screencastFrame.getData();
-						Long num = (long) Math.ceil((double) timestamp.toJson());
+						frameEpoch = (long) Math.ceil((double) timestamp.toJson());
 
-						System.err.println(String.format("timestamp: %s %s",
-								Long.toUnsignedString(num), Long.toUnsignedString(nowEpoch)));
-						Integer sesionId = screencastFrame.getSessionId();
-						byte[] image = base64.decode(data);
-						screencastFileName = "temp." + Math.ceil(Math.random() * 100)
-								+ ".png";
-						if (save) {
-							try {
-								System.err.println("Saving: " + screencastFileName);
-								FileOutputStream fileOutputStream = new FileOutputStream(
-										screencastFileName);
-								fileOutputStream.write(image);
-								fileOutputStream.close();
-							} catch (IOException e) {
-								System.err.println(
-										"Exception saving image (ignored): " + e.toString());
-							}
+						sesionId = screencastFrame.getSessionId();
+						image = base64.decode(data);
+						screencastFileName = "temp." + (frameEpoch - nowEpoch) + "."
+								+ Math.ceil(Math.random() * 100) + ".png";
+						try {
+							if (debug)
+								System.err.println(String.format("Saving frame at: %s as %s",
+										Long.toUnsignedString(frameEpoch), screencastFileName));
+							FileOutputStream fileOutputStream = new FileOutputStream(
+									screencastFileName);
+							fileOutputStream.write(image);
+							fileOutputStream.close();
+							// send ack otherwise frames stop to arrive
+							chromeDevTools.send(Page.screencastFrameAck(sesionId));
+						} catch (IOException e) {
+							System.err
+									.println("Exception saving image (ignored): " + e.toString());
 						}
 
 					} catch (WebDriverException e) {
