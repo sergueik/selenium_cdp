a0dccdb9b02439eb5c2eec81bcba436a7626c181
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2023-03-27 01:42:15 +0200

    added explicit argument for NEW_VERSION in "bump_includes.sh" to simplify switch back and forth between  Windows 7 or Windows 8.1 and Linux and Windows 10 or 11

--- bump_includes.sh
+++ bump_includes.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
-
+# NOTE: after editing on Windows, fix the line endings to prevent
+# /bin/sh^M: bad interpreter: No such file or directory error
 
 VERSION=${1:-106}
-
+NEW_VERSION=$2
 CURRENT_VERSION=$(printf "v%s" ${VERSION})
-NEW_VERSION=$(printf "v%s" $(expr 1 + ${VERSION} ))
-grep -ilr "$CURRENT_VERSION" * --include '*java' | xargs -IX sed -i "s|\.$CURRENT_VERSION|.$NEW_VERSION|g" X
-grep -ilr "${NEW_VERSION}" * --include '*java' |xargs -IX git add X
+if [ -z "$NEW_VERSION" ] ; then
+  NEW_VERSION=$(printf "v%s" $(expr 1 + ${VERSION} ))
+else
+  NEW_VERSION=$(printf "v%s" ${NEW_VERSION})
+fi
+grep -ilr "$CURRENT_VERSION" src/* --include '*java' | xargs -IX sed -i "s|\.$CURRENT_VERSION|.$NEW_VERSION|g" X
+grep -ilr "${NEW_VERSION}" src/* --include '*java' |xargs -IX git add X
