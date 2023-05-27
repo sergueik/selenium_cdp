a578c385779bbbe6ebaf8e28427e0ce807f8ca9d
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2023-05-27 21:54:13 +0200

    intermediate revision - adde relative locator test (refactored, added iterating over all elements in the order returned)

--- src/test/java/com/github/sergueik/selenium/RelativeLocatorTest.java
+++ src/test/java/com/github/sergueik/selenium/RelativeLocatorTest.java
@@ -17,22 +17,16 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.core.StringContains.containsString;
 
 import java.time.Duration;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-import org.apache.commons.codec.binary.Base64;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.locators.RelativeLocator;
 
 // see also: https://github.com/fugazi/carbonfour-selenium-4/blob/main/src/test/java/Selenium_4_Tests/TestLoginRelativeLocators.java
@@ -42,9 +36,12 @@ public class RelativeLocatorTest extends BaseCdpTest {
 	private final String header = "Student Registration Form";
 	private final String xpath = String
 			.format("//*[@id='app']//h5[contains(text(),'%s')]", header);
-	private static Base64 base64 = new Base64();
-	private static byte[] image;
-	private WebElement element;
+	private WebElement headerElement;
+	private WebElement labelElement;
+	private WebElement firstNameElement;
+	private WebElement lastNameElement;
+	private WebElement userEmailElement;
+	private List<WebElement> elements;
 
 	@Before
 	public void before() {
@@ -65,13 +62,15 @@ public class RelativeLocatorTest extends BaseCdpTest {
 	@Test
 	public void test1() {
 		// Arrange
-		WebElement headerElement = wait
+		headerElement = wait
 				.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xpath)));
+		assertThat(headerElement, notNullValue());
 		assertThat(headerElement.getText(), containsString(header));
 		Utils.highlight(headerElement);
 		System.err.println(headerElement.getText());
-		WebElement labelElement = driver.findElement(
+		labelElement = driver.findElement(
 				RelativeLocator.with(By.className("form-label")).below(headerElement));
+		assertThat(labelElement, notNullValue());
 		assertThat(labelElement.getText(), containsString("Name"));
 		Utils.highlight(labelElement);
 		System.err.println(labelElement.getText());
@@ -79,30 +78,67 @@ public class RelativeLocatorTest extends BaseCdpTest {
 		// surpsingly find the "gender-radio-1" and not the "firstName"
 		// the "placeholder" attrbute assertion will reveal "placeholder" attrbute
 		// to be blank
-		WebElement firstNameElement = driver.findElement(RelativeLocator
+		firstNameElement = driver.findElement(RelativeLocator
 				.with(By.cssSelector("input[type='text']")).toRightOf(labelElement));
+		assertThat(firstNameElement, notNullValue());
 		assertThat(firstNameElement.getAttribute("placeholder"), is("First Name"));
 		assertThat(firstNameElement.getAttribute("id"), is("firstName"));
+
 		System.err.println("id: " + firstNameElement.getAttribute("id"));
 		System.err.println(
 				"placeholder: " + firstNameElement.getAttribute("placeholder"));
 		Utils.highlight(firstNameElement);
-		WebElement lastNameElement = driver
+		lastNameElement = driver
 				.findElement(RelativeLocator.with(By.cssSelector("input[type='text']"))
 						.toRightOf(firstNameElement));
+		assertThat(lastNameElement, notNullValue());
 		assertThat(lastNameElement.getAttribute("placeholder"), is("Last Name"));
 		assertThat(lastNameElement.getAttribute("id"), is("lastName"));
 		System.err.println("id: " + lastNameElement.getAttribute("id"));
 		System.err
 				.println("placeholder: " + lastNameElement.getAttribute("placeholder"));
+		Utils.highlight(lastNameElement);
 
-		element = driver.findElement(RelativeLocator
+		userEmailElement = driver.findElement(RelativeLocator
 				.with(By.cssSelector("input[type='text']")).below(firstNameElement));
-		assertThat(element.getAttribute("placeholder"), is("name@example.com"));
-		assertThat(element.getAttribute("id"), is("userEmail"));
-		System.err.println("id: " + element.getAttribute("id"));
-		System.err.println("placeholder: " + element.getAttribute("placeholder"));
+		assertThat(userEmailElement, notNullValue());
+		assertThat(userEmailElement.getAttribute("placeholder"),
+				is("name@example.com"));
+		assertThat(userEmailElement.getAttribute("id"), is("userEmail"));
+		System.err.println("id: " + userEmailElement.getAttribute("id"));
+		System.err.println(
+				"placeholder: " + userEmailElement.getAttribute("placeholder"));
+		Utils.highlight(userEmailElement);
+		Utils.sleep(1000);
+	}
 
-		// placeholder="name@example.com" type="text" id="userEmail"
+	@Test
+	public void test2() {
+		// Arrange
+		headerElement = wait
+				.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xpath)));
+		assertThat(headerElement, notNullValue());
+		assertThat(headerElement.getText(), containsString(header));
+		Utils.highlight(headerElement);
+		System.err.println(headerElement.getText());
+		elements = driver.findElements(
+				RelativeLocator.with(By.className("form-label")).below(headerElement));
+		assertThat(elements, notNullValue());
+		elements.stream().forEach((WebElement element) -> {
+			Utils.highlight(element);
+			System.err.println("id: " + element.getAttribute("id"));
+			System.err.println("text: " + element.getText());
+			Utils.highlight(element);
+		});
+		elements = driver.findElements(RelativeLocator
+				.with(By.cssSelector("input[type='text']")).below(headerElement));
+		assertThat(elements, notNullValue());
+		elements.stream().forEach((WebElement element) -> {
+			Utils.highlight(element);
+			System.err.println("id: " + element.getAttribute("id"));
+			System.err.println("placeholder: " + element.getAttribute("placeholder"));
+			Utils.highlight(element);
+		});
+
 	}
 }
