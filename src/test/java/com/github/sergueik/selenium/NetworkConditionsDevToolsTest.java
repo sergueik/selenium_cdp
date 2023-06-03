792603aad2b11184e3e9161dbafa59fca0362a0e
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2023-06-04 00:06:39 +0200

    intermediate revision - added "Network.emulateNetworkConditions" test, simply printing the exception

---
+++ src/test/java/com/github/sergueik/selenium/NetworkConditionsDevToolsTest.java
@@ -0,0 +1,85 @@
+package com.github.sergueik.selenium;
+
+import java.io.IOException;
+import java.util.Optional;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.chromium.ChromiumNetworkConditions;
+import org.openqa.selenium.chromium.HasNetworkConditions;
+import org.openqa.selenium.devtools.v112.network.Network;
+import org.openqa.selenium.devtools.v112.network.model.ConnectionType;
+
+import org.openqa.selenium.remote.Augmenter;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+/**
+ * Selected test scenarios for Selenium Chrome Developer Tools Selenium 4 bridge
+ * based on: https://github.com/Bhakti-satalkar/junit-selenium-offline-web-cdp-se4/blob/master/src/test/java/com/lambdatest/toggleNetworkOffline.java
+ * see also:
+ * https://chromedevtools.github.io/devtools-protocol/tot/Network/#method-emulateNetworkConditions
+ * https://chromedevtools.github.io/devtools-protocol/tot/Network/#type-ConnectionType
+ * 
+ * @author: Serguei Kouzmine (kouzmine_serguei@yahoo.com)
+ */
+
+public class NetworkConditionsDevToolsTest extends BaseDevToolsTest {
+
+	private static String baseURL = "https://www.wikipedia.org";
+
+	private Boolean offline;
+
+	@After
+	public void afterTest() {
+		offline = false;
+		chromeDevTools
+				.send(Network.emulateNetworkConditions(offline, new Long(100L),
+						new Long(-1), new Long(-1), Optional.of(ConnectionType.ETHERNET)));
+
+		driver.get("about:blank");
+	}
+
+	@Test
+	public void test1() throws IOException {
+		offline = true;
+		chromeDevTools
+				.send(Network.emulateNetworkConditions(offline, new Long(100L),
+						new Long(-1), new Long(-1), Optional.of(ConnectionType.ETHERNET)));
+		try {
+			driver.get(baseURL);
+		} catch (WebDriverException e) {
+			System.err.println("Exception (ignored): " + e.toString());
+		} finally {
+			offline = false;
+			chromeDevTools
+					.send(Network.emulateNetworkConditions(offline, new Long(100L),
+							new Long(-1), new Long(-1), Optional.of(ConnectionType.NONE)));
+
+		}
+	}
+
+	@Test
+	public void test2() throws IOException {
+		WebDriver augmentedDriver = new Augmenter().augment(driver);
+		ChromiumNetworkConditions networkConditions = new ChromiumNetworkConditions();
+		networkConditions.setOffline(true);
+		((HasNetworkConditions) augmentedDriver)
+				.setNetworkConditions(networkConditions);
+
+		try {
+			driver.get(baseURL);
+		} catch (WebDriverException e) {
+			System.err.println("Exception (ignored): " + e.toString());
+		} finally {
+			((HasNetworkConditions) augmentedDriver)
+					.setNetworkConditions(new ChromiumNetworkConditions());
+		}
+
+	}
+
+}
+
