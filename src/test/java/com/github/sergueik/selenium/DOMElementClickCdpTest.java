8d915efdc4d4282d6f28eea4e7c4ee36da0698a3
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2023-10-29 16:35:04 -0400

    intermediate revision - added Click test attempt, not operational

---
+++ src/test/java/com/github/sergueik/selenium/DOMElementClickCdpTest.java
@@ -0,0 +1,210 @@
+package com.github.sergueik.selenium;
+
+/**
+ * Copyright 2023 Serguei Kouzmine
+ */
+
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasKey;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.openqa.selenium.WebDriverException;
+
+import com.google.gson.JsonSyntaxException;
+
+/**
+ * Selected test scenarios for Selenium 4 Chrome Developer Tools bridge
+ * https://chromedevtools.github.io/devtools-protocol/tot/DOM/#method-getDocument
+ * https://chromedevtools.github.io/devtools-protocol/tot/DOM/#method-querySelectorAll
+ * https://chromedevtools.github.io/devtools-protocol/tot/DOM/#method-querySelector
+ * https://chromedevtools.github.io/devtools-protocol/tot/DOM/#method-getContentQuads
+ * https://chromedevtools.github.io/devtools-protocol/tot/Input/#method-dispatchMouseEvent
+ * https://chromedevtools.github.io/devtools-protocol/tot/DOM/#type-Node
+ * @author: Serguei Kouzmine (kouzmine_serguei@yahoo.com)
+ */
+
+// based on:
+// https://github.com/estromenko/driverless-selenium/blob/init-project/driverless_selenium/webdriver.py
+public class DOMElementClickCdpTest extends BaseCdpTest {
+
+	private static String baseURL = "https://www.wikipedia.org";
+	// private final String selector = "a.other-project-link:nth-of-type(0)";
+	// private final String selector = "a.other-project-link";
+	private final String selector = "#js-link-box-en > strong";
+	// private final String selector = "#js-link-box-de > strong";
+	private static String command = "DOM.getDocument";
+
+	private static Map<String, Object> params = new HashMap<>();
+	private static Map<String, Object> result = new HashMap<>();
+	private static List<Long> results = new ArrayList<>();
+	public static Long nodeId = (long) -1;
+
+	@After
+	public void afterTest() {
+		driver.get("about:blank");
+		command = "DOM.disable";
+		result = driver.executeCdpCommand(command, new HashMap<String, Object>());
+	}
+
+	@Before
+	public void beforeTest() {
+		command = "DOM.enable";
+		params = new HashMap<String, Object>();
+		params.put("includeWhitespace", "all");
+		result = driver.executeCdpCommand(command, params);
+		driver.get(baseURL);
+	}
+
+	@SuppressWarnings("unchecked")
+	// @Ignore
+	@Test
+	public void test1() {
+
+		try {
+			command = "DOM.getDocument";
+			params = new HashMap<>();
+			params.put("pierce", false);
+			params.put("depth", 1);
+			// Act
+			result = driver.executeCdpCommand(command, params);
+			nodeId = Long.parseLong(
+					((Map<String, Object>) result.get("root")).get("nodeId").toString());
+			command = "DOM.querySelectorAll";
+			params.clear();
+			params.put("nodeId", nodeId);
+			params.put("selector", selector);
+
+			result = driver.executeCdpCommand(command, params);
+			assertThat(result, hasKey("nodeIds"));
+			results = (List<Long>) result.get("nodeIds");
+			// Assert
+			assertThat(results, notNullValue());
+			assertTrue(results.size() != 0);
+			System.err.println(String.format("Found %d Node Ids:", results.size()));
+			results.subList(0, 1).forEach(nodeId -> {
+				command = "DOM.getOuterHTML";
+				params.clear();
+				params.put("nodeId", nodeId);
+				result = driver.executeCdpCommand(command, params);
+				assertThat(result, notNullValue());
+				assertTrue(result.containsKey("outerHTML"));
+				System.err.println(String.format("Id: %s\nHTML: %s", nodeId.toString(),
+						result.get("outerHTML")));
+				command = "DOM.getContentQuads";
+				params.clear();
+				params.put("nodeId", nodeId);
+				result = driver.executeCdpCommand(command, params);
+				assertThat(result, notNullValue());
+				assertTrue(result.containsKey("quads"));
+				System.err.println(String.format("Id: %s\nquads: %s", nodeId.toString(),
+						result.get("quads")));
+				List<List<Double>> data1 = (List<List<Double>>) result.get("quads");
+				// NOTE: sometimes observed
+				// java.lang.ClassCastException: java.lang.Long cannot be cast to
+				// java.lang.Double
+				// quads: [[299, 167.4499969482422, 455, 167.4499969482422, 455,
+				// 191.4499969482422,299, 191.4499969482422]]
+				List<Double> data2 = (List<Double>) data1.get(0);
+				int x = Math.round(data2.get(0).longValue());
+				int y = Math.round(data2.get(1).longValue());
+				// System.err.println(String.format("Click at x: %.2f y: %.2f", x, y));
+				System.err.println(String.format("Click at x: %d y: %d", x, y));
+				command = "Input.dispatchMouseEvent";
+				params.clear();
+				params.put("x", x);
+				params.put("y", y);
+				params.put("type", "mousePressed");
+				driver.executeCdpCommand(command, params);
+				Utils.sleep(100);
+				params.clear();
+				params.put("x", x);
+				params.put("y", y);
+				params.put("type", "mouseReleased");
+				driver.executeCdpCommand(command, params);
+				Utils.sleep(10000);
+			});
+
+		} catch (JsonSyntaxException e) {
+			System.err.println("JSON Syntax exception in " + command + " (ignored): "
+					+ e.toString());
+		} catch (WebDriverException e) {
+			System.err.println(
+					"Web Driver exception in " + command + " (ignored): " + e.getMessage()
+							+ " " + Utils.processExceptionMessage(e.getMessage()));
+		} catch (Exception e) {
+			System.err.println("Exception in " + command + "  " + e.toString());
+			e.printStackTrace();
+			throw (new RuntimeException(e));
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	@Ignore
+	@Test
+	public void test2() {
+
+		try {
+			command = "DOM.performSearch";
+			params = new HashMap<>();
+			params.put("query", selector);
+			params.put("includeUserAgentShadowDOM", false);
+			// Act
+			result = driver.executeCdpCommand(command, params);
+			assertThat(results, notNullValue());
+			assertThat(result, hasKey("searchId"));
+			assertThat(result, hasKey("resultCount"));
+			int resultCount = Integer.parseInt(result.get("resultCount").toString());
+			System.err.println(String.format("Search %s Has %d results:",
+					result.get("searchId").toString(), resultCount));
+			command = "DOM.getSearchResults";
+			params.clear();
+			params.put("searchId", result.get("searchId"));
+			params.put("fromIndex", 0);
+			params.put("toIndex", resultCount - 1);
+
+			result = driver.executeCdpCommand(command, params);
+			assertThat(result, hasKey("nodeIds"));
+			results = (List<Long>) result.get("nodeIds");
+			// Assert
+			assertThat(results, notNullValue());
+			assertTrue(results.size() != 0);
+			System.err.println(
+					String.format("Found %d Node Ids: %s", results.size(), results)
+							+ ", explore first one");
+			/*
+			results.forEach(nodeId -> {
+				command = "DOM.getOuterHTML";
+				params.clear();
+				params.put("nodeId", nodeId);
+				result = driver.executeCdpCommand(command, params);
+				assertThat(result, notNullValue());
+				assertTrue(result.containsKey("outerHTML"));
+				System.err.println(String.format("Id: %s\nHTML: %s", nodeId.toString(),
+						result.get("outerHTML")));
+			});
+			*/
+
+		} catch (JsonSyntaxException e) {
+			System.err.println("JSON Syntax exception in " + command + " (ignored): "
+					+ e.toString());
+		} catch (WebDriverException e) {
+			System.err.println("Web Driver exception in " + command + " (ignored): "
+					+ Utils.processExceptionMessage(e.getMessage()));
+		} catch (Exception e) {
+			System.err.println("Exception in " + command + "  " + e.toString());
+			e.printStackTrace();
+			throw (new RuntimeException(e));
+		}
+	}
+}
