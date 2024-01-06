3677efc1cff670ff2be8484723fa32df42f9f487
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2024-01-06 01:46:48 +0100

    intermediate revision - added "navigator.userAgentData" dev tools test

--- src/test/java/com/github/sergueik/selenium/UserAgentOverrideDevToolsTest.java
+++ src/test/java/com/github/sergueik/selenium/UserAgentOverrideDevToolsTest.java
@@ -1,5 +1,6 @@
 package com.github.sergueik.selenium;
 
+/* Copyright 2023,2024 Serguei Kouzmine */
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -8,19 +9,31 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import org.junit.Assume;
+import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.devtools.idealized.Network.UserAgent;
 import org.openqa.selenium.devtools.v120.emulation.Emulation;
-//import org.openqa.selenium.devtools.Console;
+import org.openqa.selenium.devtools.v120.emulation.model.UserAgentMetadata;
+import org.openqa.selenium.devtools.v120.emulation.model.UserAgentBrandVersion;
+
+// import org.openqa.selenium.devtools.Console;
 // import org.openqa.selenium.devtools.Log;
 import org.openqa.selenium.devtools.v120.network.Network;
 
 /**
  * Selected test scenarios for Selenium Chrome Developer Tools Selenium 4 bridge
  * https://chromedevtools.github.io/devtools-protocol/tot/Network/#method-setUserAgentOverride
+ * https://chromedevtools.github.io/devtools-protocol/tot/Emulation/#method-setUserAgentOverride
+ * https://chromedevtools.github.io/devtools-protocol/tot/Emulation/#type-UserAgentMetadata
+ * https://chromedevtools.github.io/devtools-protocol/tot/Emulation/#type-UserAgentBrandVersion
+ * https://wicg.github.io/ua-client-hints/
+ * https://javadoc.io/static/org.seleniumhq.selenium/selenium-devtools-v119/4.16.1/org/openqa/selenium/devtools/v119/emulation/Emulation.html#setUserAgentOverride(java.lang.String,java.util.Optional,java.util.Optional,java.util.Optional)
+ * https://www.lambdatest.com/automation-testing-advisor/selenium/methods/org.openqa.selenium.devtools.idealized.Network.UserAgent.acceptLanguage
  * @author: Serguei Kouzmine (kouzmine_serguei@yahoo.com)
  */
 
@@ -28,14 +41,30 @@ public class UserAgentOverrideDevToolsTest extends BaseDevToolsTest {
 
 	private static WebElement element = null;
 	private static List<WebElement> elements = new ArrayList<>();
-	private static By locator = By.xpath(
+	private static final By locator = By.xpath(
 			"//*[@id=\"content-base\"]//table//th[contains(text(),\"USER-AGENT\")]/../td");
+	private static final By locator2 = By
+			.cssSelector("#content-base div.content-block-main");
 
 	private static String baseURL = "https://www.whatismybrowser.com/detect/what-http-headers-is-my-browser-sending";
 
 	private final static int id = (int) (java.lang.Math.random() * 1_000_000);
 	public final static String consoleMessage = "message from test id #" + id;
 
+	@Before
+	public void before() throws Exception {
+		// @formatter:off
+		chromeDevTools.send(
+				Emulation.setUserAgentOverride(
+						"", // userAgent 
+						Optional.empty(), // acceptLanguage
+						Optional.empty(), // platform
+						Optional.empty() // userAgentMetadata
+				));
+		// @formatter:on
+	}
+
+	// @Ignore
 	@Test
 	public void test1() {
 		// the site may be down, and it can also reject autoated browsing
@@ -43,10 +72,9 @@ public class UserAgentOverrideDevToolsTest extends BaseDevToolsTest {
 		// Assume.assumeTrue(pingHost("www.whoishostingthis.com", 443, 3));
 		// Arrange
 		driver.get(baseURL);
-		locator = By.cssSelector("#content-base div.content-block-main");
-		elements = driver.findElements(locator);
-		if (elements.size() > 0) {
-			// You have been blocked
+		elements = driver.findElements(locator2);
+		if (elements.size() == 0) {
+			// // You have been blocked ?
 			return;
 		}
 
@@ -93,11 +121,88 @@ public class UserAgentOverrideDevToolsTest extends BaseDevToolsTest {
 		// pingHost() does not work reliably yet
 		// Assume.assumeTrue(pingHost("www.whoishostingthis.com", 443, 3));
 		// Arrange
+		String brand = "Chrome";
+		String version = "120";
+		String platform = "windows";
 		driver.get(baseURL);
-		locator = By.cssSelector("#content-base div.content-block-main");
-		elements = driver.findElements(locator);
-		if (elements.size() > 0) {
-			// You have been blocked
+		elements = driver.findElements(locator2);
+		if (elements.size() == 0) {
+			// You have been blocked ?
+			return;
+		}
+
+		element = driver.findElement(locator);
+		Utils.highlight(element);
+		Utils.sleep(100);
+		assertThat(element.getAttribute("innerText"), containsString("Mozilla"));
+		System.err
+				.println("Vanilla USER-AGENT: " + element.getAttribute("innerText"));
+
+		// Act
+		try {
+			UserAgentBrandVersion userAgentBrandVersion = new UserAgentBrandVersion(
+					brand, version);
+			// version is required
+			List<UserAgentBrandVersion> brands = new ArrayList<>();
+			brands.add(userAgentBrandVersion);
+			// @formatter:off
+			// Emulation.UserAgentMetadata
+			// To be sent in Sec-CH-UA-* headers and returned in navigator.userAgentData
+			// EXPERIMENTAL
+			UserAgentMetadata userAgentMetadata = new UserAgentMetadata(
+					Optional.of(brands), 
+					Optional.empty(), // fullVersionList 
+					Optional.empty(), // fullVersion 
+					platform, 
+					"NT 6.0", //  platformVersion 
+					"amd64", // architecture 
+					"", // model cannot be null (java.lang.NullPointerException)
+					false, // mobile 
+					Optional.empty(), // bitness, 
+					Optional.of(false) // wow64
+					);
+			// @formatter:on
+			UserAgent userAgent = new UserAgent(brand);
+			userAgent.platform(platform);
+			// @formatter:off
+			chromeDevTools.send(
+					Emulation.setUserAgentOverride(
+							userAgent.userAgent(), // userAgent 
+							Optional.empty(), // acceptLanguage
+							Optional.empty(), // platform
+							Optional.of(userAgentMetadata)) // userAgentMetadata
+					);
+			// @formatter:on
+		} catch (WebDriverException e) {
+			System.err.println("Web Driver exception (ignored): "
+					+ Utils.processExceptionMessage(e.getMessage()));
+		} catch (Exception e) {
+			System.err.println("Exception " + e.toString());
+			throw (new RuntimeException(e));
+		}
+		driver.navigate().refresh();
+		Utils.sleep(1000);
+
+		element = driver.findElement(locator);
+		assertThat(element.isDisplayed(), is(true));
+		assertThat(element.getAttribute("innerText"), is(brand));
+		System.err
+				.println("Updated USER-AGENT: " + element.getAttribute("innerText"));
+	}
+
+	// @Ignore
+	@Test
+	public void test3() {
+		// the site may be down, and it can also reject autoated browsing
+		// pingHost() does not work reliably yet
+		// Trying to connect to host www.whoishostingthis.com port 443
+		// timeout or unreachable or failed DNS lookup.
+		// Assume.assumeTrue(pingHost("www.whoishostingthis.com", 443, 3));
+		// Arrange
+		driver.get(baseURL);
+		elements = driver.findElements(locator2);
+		if (elements.size() == 0) {
+			// You have been blocked ?
 			return;
 		}
 
