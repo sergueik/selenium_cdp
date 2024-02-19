2807ead08affd12e30da57293713d61650113a97
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2024-02-20 00:52:45 +0100

    intermediate revision - added Console log test through Runtime.consoleAPICalled method

---
+++ src/test/java/com/github/sergueik/selenium/ConsoleApiCalledLoggingDevToolsTest.java
@@ -0,0 +1,71 @@
+package com.github.sergueik.selenium;
+
+/**
+ * Copyright 2024 Serguei Kouzmine
+ */
+
+import java.time.Duration;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.devtools.v121.runtime.Runtime;
+import org.openqa.selenium.devtools.v121.runtime.model.RemoteObject;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+/**
+ * Selected test scenarios for Selenium Chrome Developer Tools Selenium 4 bridge
+ * based on:
+ * https://www.selenium.dev/documentation/webdriver/bidirectional/chrome_devtools/cdp_api/#console-logs
+ * see also:
+ * https://chromedevtools.github.io/devtools-protocol/tot/Runtime/#event-consoleAPICalled
+ * https://chromedevtools.github.io/devtools-protocol/tot/Runtime/#type-RemoteObject
+ * @author: Serguei Kouzmine (kouzmine_serguei@yahoo.com)
+ */
+
+@SuppressWarnings("deprecation")
+public class ConsoleApiCalledLoggingDevToolsTest extends BaseDevToolsTest {
+
+	private WebElement element;
+	protected static WebDriverWait wait;
+	public final static int flexibleWait = 10;
+	public final static Duration duration = Duration.ofSeconds(flexibleWait);
+	public final static int implicitWait = 1;
+	public final static int pollingInterval = 50;
+
+	private final static String baseURL = "https://www.selenium.dev/selenium/web/bidi/logEntryAdded.html";
+	private CopyOnWriteArrayList<String> logs = new CopyOnWriteArrayList<>();
+
+	@Before
+	public void beforeTest() throws Exception {
+		wait = new WebDriverWait(driver, duration);
+		wait.pollingEvery(Duration.ofMillis(pollingInterval));
+		chromeDevTools.send(Runtime.enable());
+		driver.get(baseURL);
+	}
+
+	@Test
+	public void test1() {
+
+		chromeDevTools.addListener(Runtime.consoleAPICalled(), event -> logs
+				.add((String) event.getArgs().get(0).getValue().orElse("")));
+
+		element = wait.until(
+				ExpectedConditions.visibilityOfElementLocated(By.id("consoleLog")));
+		element.click();
+		Utils.sleep(1000);
+		logs.stream().forEach(System.err::println);
+	}
+
+	@After
+	public void afterTest() throws Exception {
+		logs.clear();
+		chromeDevTools.clearListeners();
+		chromeDevTools.send(Runtime.disable());
+	}
+
+}
