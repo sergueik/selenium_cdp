d490f480072bd345374e1505b68c81e2720d9c7c
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2024-04-01 02:56:08 +0200

    merged  two smallvariations of "BrowserDownloadDevToolsTest.java"

--- src/test/java/com/github/sergueik/selenium/BrowserDownloadDevToolsTest.java
+++ src/test/java/com/github/sergueik/selenium/BrowserDownloadDevToolsTest.java
@@ -1,6 +1,7 @@
 package com.github.sergueik.selenium;
+
 /**
- * Copyright 2023-2024 Serguei Kouzmine
+ * Copyright 2023,2024 Serguei Kouzmine
  */
 
 import static org.hamcrest.CoreMatchers.is;
@@ -24,7 +25,6 @@ import org.openqa.selenium.WebDriverException;
 // NOTE: will need to switch to v113 for Java 1.8 testing (later releases of Selenium are compiled to a later jar format
 // this will lead to ignorable warning:
 // Unable to find an exact match for CDP version 119, so returning the closest version found: 115
-
 import org.openqa.selenium.devtools.v123.browser.Browser;
 import org.openqa.selenium.devtools.v123.browser.model.DownloadProgress;
 import org.openqa.selenium.devtools.v123.browser.model.DownloadWillBegin;
@@ -44,13 +44,11 @@ public class BrowserDownloadDevToolsTest extends BaseDevToolsTest {
 	// because of the already provided
 	// "browser.helperApps.neverAsk.saveToDisk" argument
 	// including "application/pdf"
-
 	private final static String url = "https://scholar.harvard.edu/files/torman_personal/files/samplepptx.pptx";
 	private final static String filename = url.replaceAll("^.*/", "");
 	private static String tempDownloadDirectory = getTempDownloadDirectory();
-	// private static BrowserContextID browserContextID = "";
 
-	private static String downloadPath = null;
+	// private static BrowserContextID browserContextID = "";
 
 	@Before
 	public void beforeTest() throws Exception {
@@ -63,8 +61,7 @@ public class BrowserDownloadDevToolsTest extends BaseDevToolsTest {
 				Browser.setDownloadBehavior(Browser.SetDownloadBehaviorBehavior.DEFAULT,
 						Optional.empty(), Optional.empty(), Optional.of(false)));
 		try {
-			Files.delete((Paths.get(Paths.get(downloadPath).resolve(filename)
-					.toAbsolutePath().toString())));
+			Files.delete(Paths.get(tempDownloadDirectory).resolve(filename));
 			// Files.delete(Paths.get(downloadPath));
 		} catch (IOException e) {
 		}
@@ -73,34 +70,34 @@ public class BrowserDownloadDevToolsTest extends BaseDevToolsTest {
 	@Test
 	public void test1() {
 		// Arrange
-		downloadPath = createTempDownloadDir();
-		System.err.println("Downloading to " + downloadPath);
-		chromeDevTools.send(
-				Browser.setDownloadBehavior(Browser.SetDownloadBehaviorBehavior.ALLOW,
-						Optional.empty(), Optional.of(downloadPath), Optional.of(true)));
+		tempDownloadDirectory = getTempDownloadDirectory();
+		chromeDevTools.send(Browser.setDownloadBehavior(
+				Browser.SetDownloadBehaviorBehavior.ALLOW, Optional.empty(),
+				Optional.of(tempDownloadDirectory), Optional.of(true)));
 		driver.get(url);
 		Utils.sleep(3000);
-		assertThat(new File(
-				Paths.get(downloadPath).resolve(filename).toAbsolutePath().toString())
-						.exists(),
-				is(true));
+		assertThat(new File(Paths.get(tempDownloadDirectory).resolve(filename)
+				.toAbsolutePath().toString()).exists(), is(true));
 		System.err.println(String.format("Verified downloaded file: %s in %s",
-				filename, downloadPath));
-
+				filename, tempDownloadDirectory));
 	}
 
 	@Test
 	public void test2() {
-		downloadPath = createTempDownloadDir();
-		System.err.println("Downloading to " + downloadPath);
 		List<DownloadProgress.State> states = new ArrayList<>();
+
+		tempDownloadDirectory = getTempDownloadDirectory();
 		// Arrange
-		chromeDevTools.send(
-				Browser.setDownloadBehavior(Browser.SetDownloadBehaviorBehavior.ALLOW,
-						Optional.empty(), Optional.of(downloadPath), Optional.of(true)));
+		chromeDevTools.send(Browser.setDownloadBehavior(
+				Browser.SetDownloadBehaviorBehavior.ALLOW, Optional.empty(),
+				Optional.of(tempDownloadDirectory), Optional.of(true)));
 
 		// Act
 		try {
+			// NOTE: the explicit type declaration of lambda argument of
+			// org.openqa.selenium.devtools.DevTools.addListener(
+			// Event<DownloadWillBegin> event, Consumer<DownloadWillBegin> handler)
+			// is optional
 			chromeDevTools.addListener(Browser.downloadWillBegin(),
 					(DownloadWillBegin o) -> {
 						System.err.println("in Browser.downloadWillBegin listener. " + "\t"
@@ -116,10 +113,8 @@ public class BrowserDownloadDevToolsTest extends BaseDevToolsTest {
 					});
 			driver.get(url);
 			Utils.sleep(3000);
-			assertThat(new File(
-					Paths.get(downloadPath).resolve(filename).toAbsolutePath().toString())
-							.exists(),
-					is(true));
+			assertThat(new File(Paths.get(tempDownloadDirectory).resolve(filename)
+					.toAbsolutePath().toString()).exists(), is(true));
 			// If the event is not fired, will encounter AssertionError
 			// Allowed Values: inProgress, completed, canceled
 			assertThat("Looking for COMPLETED events",
@@ -144,25 +139,110 @@ public class BrowserDownloadDevToolsTest extends BaseDevToolsTest {
 						.toAbsolutePath().toString();
 	}
 
-	// http://www.java2s.com/Code/Java/JDK-7/Createtempfileanddirectory.htm
-	public static String createTempDownloadDir() {
+	@Test
+	public void test3() {
+		final List<String> filenames = new ArrayList<>();
+		final List<DownloadProgress.State> states = new ArrayList<>();
+		// Arrange
+		chromeDevTools.send(Browser.setDownloadBehavior(
+				Browser.SetDownloadBehaviorBehavior.ALLOWANDNAME, Optional.empty(),
+				Optional.of(tempDownloadDirectory), Optional.of(true)));
 
-		String tempDownloadDirPath = null;
-
+		// Act
 		try {
-			Path tempDirectory = Files.createTempDirectory(
-					FileSystems.getDefault().getPath(getTempDownloadDir()), "");
-			System.err.println("Temporary Download directory created");
-			tempDownloadDirPath = tempDirectory.toString();
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
-		return tempDownloadDirPath;
+			chromeDevTools.addListener(Browser.downloadWillBegin(), o -> {
+				String filename = o.getSuggestedFilename();
+				// NOTE: this is not what the downloaded file be named
+				System.err.println("in Browser.downloadWillBegin listener. url: "
+						+ o.getUrl() + "\tfilename: " + filename);
+			});
+			chromeDevTools.addListener(Browser.downloadProgress(), o -> {
+				DownloadProgress.State state = o.getState();
+				String filename = o.getGuid();
+				// NOTE: java.util.UnknownFormatConversionException: Conversion = 'l'
+				System.err.println(
+						"in Browser.downloadProgress listener. state: " + state.toString());
+				if (o.getTotalBytes().doubleValue() > 0f)
+					System.err.println(String.format("%8.2f %% downloaded.",
+							Math.ceil(100.0 * o.getReceivedBytes().doubleValue()
+									/ o.getTotalBytes().doubleValue())));
 
+				states.add(state);
+				if (state == DownloadProgress.State.COMPLETED)
+					filenames.add(filename);
+			});
+			driver.get(url);
+			Utils.sleep(3000);
+			assertThat(states.indexOf(DownloadProgress.State.COMPLETED),
+					greaterThan(-1));
+			assertThat(filenames.size(), is(1));
+			String downloadedFilename = filenames.get(0);
+			System.err
+					.println("Inspecting downloaded filename: " + downloadedFilename);
+			assertThat(new File(Paths.get(tempDownloadDirectory)
+					.resolve(downloadedFilename).toAbsolutePath().toString()).exists(),
+					is(true));
+			System.err.println(String.format("Verified downloaded file: %s in %s",
+					downloadedFilename, tempDownloadDirectory));
+			new File(Paths.get(tempDownloadDirectory).resolve(downloadedFilename)
+					.toAbsolutePath().toString()).delete();
+		} catch (WebDriverException e) {
+			System.err.println("Web Driver exception (ignored): "
+					+ Utils.processExceptionMessage(e.getMessage()));
+		} catch (Exception e) {
+			System.err.println("Exception: " + e.toString());
+			throw (new RuntimeException(e));
+		}
+	}
+
+	// see also:
+	// https://chromedevtools.github.io/devtools-protocol/tot/Browser/#method-cancelDownload
+	// Browser.cancelDownload
+
+	@Test
+	public void test4() {
+		List<DownloadProgress.State> states = new ArrayList<>();
+		// Arrange
+		chromeDevTools.send(Browser.setDownloadBehavior(
+				Browser.SetDownloadBehaviorBehavior.ALLOW, Optional.empty(),
+				Optional.of(tempDownloadDirectory), Optional.of(true)));
+
+		// Act
+		try {
+			chromeDevTools.addListener(Browser.downloadWillBegin(), o -> {
+				System.err.println("in Browser.downloadWillBegin listener. url: "
+						+ o.getUrl() + "\tfilename: " + o.getSuggestedFilename());
+			});
+			chromeDevTools.addListener(Browser.downloadProgress(), o -> {
+				DownloadProgress.State state = o.getState();
+				System.err.println(
+						"in Browser.downloadProgress listener. state: " + state.toString());
+				states.add(state);
+
+				System.err.println("Cancel download: " + o.getGuid());
+
+				chromeDevTools
+						.send(Browser.cancelDownload(o.getGuid(), Optional.empty()));
+
+			});
+			driver.get(url);
+			Utils.sleep(3000);
+			assertThat(states.indexOf(DownloadProgress.State.COMPLETED), is(-1));
+			assertThat(states.indexOf(DownloadProgress.State.CANCELED),
+					greaterThan(-1));
+
+		} catch (WebDriverException e) {
+			System.err.println("Web Driver exception (ignored): "
+					+ Utils.processExceptionMessage(e.getMessage()));
+		} catch (Exception e) {
+			System.err.println("Exception: " + e.toString());
+			throw (new RuntimeException(e));
+		}
 	}
 
 	// TODO: shadow DOM test
 	// https://stackoverflow.com/questions/57780426/selenium-headless-chrome-how-to-query-status-of-downloads
+
 	// http://www.java2s.com/example/java-utility-method/temp-directory-get/gettempdir-466ee.html
 	public static String getTempDownloadDirectory() {
 		String tempDownloadDirectory = System.getProperty("java.io.tmpdir");
