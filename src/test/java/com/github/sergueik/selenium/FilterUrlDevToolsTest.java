2e4764c23cffed5b38a12fe387c87de1dd7771a9
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2024-07-13 00:15:27 +0200

    intermedite revision - collect requestId to correlate

--- src/test/java/com/github/sergueik/selenium/FilterUrlDevToolsTest.java
+++ src/test/java/com/github/sergueik/selenium/FilterUrlDevToolsTest.java
@@ -4,7 +4,6 @@ package com.github.sergueik.selenium;
  * Copyright 2021,2024 Serguei Kouzmine
  */
 
-
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -14,12 +13,15 @@ import java.util.Optional;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.openqa.selenium.devtools.v126.network.Network;
 import org.openqa.selenium.devtools.v126.network.model.BlockedReason;
 import org.openqa.selenium.devtools.v126.network.model.Headers;
 import org.openqa.selenium.devtools.v126.network.model.InterceptionStage;
 import org.openqa.selenium.devtools.v126.network.model.LoadingFailed;
+import org.openqa.selenium.devtools.v126.network.model.Request;
+import org.openqa.selenium.devtools.v126.network.model.RequestId;
 import org.openqa.selenium.devtools.v126.network.model.RequestIntercepted;
 import org.openqa.selenium.devtools.v126.network.model.RequestPattern;
 import org.openqa.selenium.devtools.v126.network.model.RequestWillBeSent;
@@ -31,14 +33,23 @@ import com.google.common.collect.ImmutableList;
 
 /**
  * Selected test scenarios for Selenium Chrome Developer Tools Selenium 4 bridge
- * https://chromedevtools.github.io/devtools-protocol/tot/Network/#method-setBlockedURLs
- * https://chromedevtools.github.io/devtools-protocol/tot/Network/#event-loadingFailed
- * https://chromedevtools.github.io/devtools-protocol/tot/Network/#event-requestWillBeSent
- * https://chromedevtools.github.io/devtools-protocol/tot/Network/#event-responseReceived
- * https://chromedevtools.github.io/devtools-protocol/tot/Network/#method-continueInterceptedRequest
- * https://chromedevtools.github.io/devtools-protocol/tot/Network/#type-RequestPattern
- * https://chromedevtools.github.io/devtools-protocol/tot/Network/#method-setCacheDisabled
- * https://chromedevtools.github.io/devtools-protocol/tot/Network/#method-clearBrowserCache
+ * https://chromedevtools.github.io/devtools-protocol/tot/Network/#method-
+ * setBlockedURLs
+ * https://chromedevtools.github.io/devtools-protocol/tot/Network/#event-
+ * loadingFailed
+ * https://chromedevtools.github.io/devtools-protocol/tot/Network/#event-
+ * requestWillBeSent
+ * https://chromedevtools.github.io/devtools-protocol/tot/Network/#event-
+ * responseReceived
+ * https://chromedevtools.github.io/devtools-protocol/tot/Network/#method-
+ * continueInterceptedRequest
+ * https://chromedevtools.github.io/devtools-protocol/tot/Network/#type-
+ * RequestPattern
+ * https://chromedevtools.github.io/devtools-protocol/tot/Network/#method-
+ * setCacheDisabled
+ * https://chromedevtools.github.io/devtools-protocol/tot/Network/#method-
+ * clearBrowserCache
+ * 
  * @author: Serguei Kouzmine (kouzmine_serguei@yahoo.com)
  */
 
@@ -46,8 +57,7 @@ public class FilterUrlDevToolsTest extends BaseDevToolsTest {
 
 	@Before
 	public void before() throws Exception {
-		chromeDevTools.send(Network.enable(Optional.of(100000000), Optional.empty(),
-				Optional.empty()));
+		chromeDevTools.send(Network.enable(Optional.of(100000000), Optional.empty(), Optional.empty()));
 		chromeDevTools.send(Network.clearBrowserCache());
 		chromeDevTools.send(Network.setCacheDisabled(true));
 		baseURL = "http://arngren.net";
@@ -72,8 +82,13 @@ public class FilterUrlDevToolsTest extends BaseDevToolsTest {
 				ImmutableList.of("*.css", "*.png", "*.jpg", "*.gif", "*favicon.ico")));
 
 		// verify that css jpg and png are blocked
+		// see also:
+		// https://rahulshettyacademy.com/blog/index.php/2021/11/04/selenium-4-key-feature-network-interception/
+		// https://javadoc.io/static/org.seleniumhq.selenium/selenium-devtools-v120/4.18.1/org/openqa/selenium/devtools/v126/network/model/LoadingFailed.html
 		chromeDevTools.addListener(Network.loadingFailed(),
 				(LoadingFailed event) -> {
+					//
+					RequestId requestId = event.getRequestId();
 					ResourceType resourceType = event.getType();
 					if (resourceType.equals(ResourceType.STYLESHEET)
 							|| resourceType.equals(ResourceType.IMAGE)
@@ -82,21 +97,20 @@ public class FilterUrlDevToolsTest extends BaseDevToolsTest {
 						assertThat(blockedReason.isPresent(), is(true));
 						assertThat(blockedReason.get(), is(BlockedReason.INSPECTOR));
 					} else {
-						// TODO
+						System.err.println(String.format("Also Blocked request %s event type: %s", requestId, resourceType));
 					}
-					System.err.println("Blocked event: " + event.getType());
+					System.err.println(String.format("Blocked request %s event type: %s", requestId, resourceType));
 				});
 
 		chromeDevTools.addListener(Network.requestWillBeSent(),
 				(RequestWillBeSent event) -> {
-					System.err.println("Request will be sent to get url: "
+			System.err.println("Request will be sent to get url: "
 							+ event.getRequest().getUrl());
 
 				});
 		chromeDevTools.addListener(Network.responseReceived(),
 				(ResponseReceived event) -> {
-					System.err.println(
-							"Response received with url: " + event.getResponse().getUrl());
+			System.err.println( "Response received with url: " + event.getResponse().getUrl());
 
 				}); // Act
 		// see also:
@@ -111,6 +125,7 @@ public class FilterUrlDevToolsTest extends BaseDevToolsTest {
 	// https://rahulshettyacademy.com/blog/index.php/2021/11/04/selenium-4-key-feature-network-interception/
 	// NOTE: need to print to read the whole document
 	@SuppressWarnings("deprecation")
+	@Ignore
 	@Test
 	public void test2() {
 		// TODO: java.lang.ClassCastException:
@@ -118,20 +133,16 @@ public class FilterUrlDevToolsTest extends BaseDevToolsTest {
 		// cast to java.lang.Void
 		chromeDevTools.addListener(Network.requestIntercepted(),
 				(RequestIntercepted event) -> chromeDevTools
-						.send(Network.continueInterceptedRequest(event.getInterceptionId(),
-								Optional.empty(), Optional.empty(), Optional.empty(),
-								Optional.empty(), Optional.empty(),
-								Optional.of(new Headers(new HashMap<String, Object>())),
-								Optional.empty())));
+						.send(Network.continueInterceptedRequest(event.getInterceptionId(), Optional.empty(),
+								Optional.empty(), Optional.empty(), Optional.empty(), Optional.empty(),
+								Optional.of(new Headers(new HashMap<String, Object>())), Optional.empty())));
 
 		// set request interception only for css requests
-		RequestPattern requestPattern = new RequestPattern(Optional.of("*.gif"),
-				Optional.of(ResourceType.IMAGE),
+		RequestPattern requestPattern = new RequestPattern(Optional.of("*.gif"), Optional.of(ResourceType.IMAGE),
 				Optional.of(InterceptionStage.HEADERSRECEIVED));
+		chromeDevTools.send(Network.setRequestInterception(ImmutableList.of(requestPattern)));
 		chromeDevTools
-				.send(Network.setRequestInterception(ImmutableList.of(requestPattern)));
-		chromeDevTools.send(Page.navigate(baseURL, Optional.empty(),
-				Optional.empty(), Optional.empty(), Optional.empty()));
+				.send(Page.navigate(baseURL, Optional.empty(), Optional.empty(), Optional.empty(), Optional.empty()));
 		Utils.sleep(1000);
 	}
 
