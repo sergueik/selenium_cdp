477604a4a02c9a113099a5c775e16ab99757bcf9
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2024-07-21 01:03:55 +0200

    intermediate revision - fxed "DOMElementClickDevToolsTest" test

--- src/test/java/com/github/sergueik/selenium/DOMElementClickDevToolsTest.java
+++ src/test/java/com/github/sergueik/selenium/DOMElementClickDevToolsTest.java
@@ -15,8 +15,10 @@ import org.junit.Test;
 import org.openqa.selenium.devtools.DevToolsException;
 import org.openqa.selenium.devtools.v126.dom.DOM;
 import org.openqa.selenium.devtools.v126.input.Input;
+
+import com.google.gson.Gson;
+
 import org.openqa.selenium.devtools.v126.dom.DOM.EnableIncludeWhitespace;
-import org.openqa.selenium.devtools.v126.dom.DOM.PerformSearchResponse;
 import org.openqa.selenium.devtools.v126.dom.model.Node;
 import org.openqa.selenium.devtools.v126.dom.model.NodeId;
 import org.openqa.selenium.devtools.v126.dom.model.Quad;
@@ -25,13 +27,13 @@ import org.openqa.selenium.devtools.v126.dom.model.Quad;
  * Selected test scenarios for Selenium 4 Chrome Developer Tools bridge
  * https://chromedevtools.github.io/devtools-protocol/tot/DOM/#method-getDocument
  * https://chromedevtools.github.io/devtools-protocol/tot/DOM/#method-querySelectorAll
- * https://chromedevtools.github.io/devtools-protocol/tot/DOM/#method-querySelector 
+ * https://chromedevtools.github.io/devtools-protocol/tot/DOM/#method-querySelector
  * https://chromedevtools.github.io/devtools-protocol/tot/DOM/#method-getContentQuads
- * https://chromedevtools.github.io/devtools-protocol/tot/DOM/#type-Quad
- * https://chromedevtools.github.io/devtools-protocol/tot/Input/#method-dispatchMouseEvent
+ * https://chromedevtools.github.io/devtools-protocol/tot/DOM/#type-Quadhttps://chromedevtools.github.io/devtools-protocol/tot/Input/#method-dispatchMouseEvent
  * https://chromedevtools.github.io/devtools-protocol/tot/DOM/#method-performSearch
  * https://chromedevtools.github.io/devtools-protocol/tot/DOM/#type-Node
  * https://chromedevtools.github.io/devtools-protocol/1-3/DOM/#event-setChildNodes
+ * 
  * @author: Serguei Kouzmine (kouzmine_serguei@yahoo.com)
  */
 
@@ -46,6 +48,7 @@ public class DOMElementClickDevToolsTest extends BaseDevToolsTest {
 	// cmd=DOM.querySelectorAll}
 
 	private final String selector = "a.other-project-link";
+	private Gson gson = new Gson();
 
 	@After
 	public void afterTest() {
@@ -61,61 +64,55 @@ public class DOMElementClickDevToolsTest extends BaseDevToolsTest {
 	}
 
 	@SuppressWarnings("unchecked")
-	@Test(expected = DevToolsException.class)
+	// @Test(expected = DevToolsException.class)
+	@Test
 	public void test1() {
 		try {
-			Node result = chromeDevTools
-					.send(DOM.getDocument(Optional.of(1), Optional.of(true)));
+			Node result = chromeDevTools.send(DOM.getDocument(Optional.of(1), Optional.of(true)));
 
 			// Limit example to just one node
-			List<NodeId> results = chromeDevTools
-					.send(DOM.querySelectorAll(result.getNodeId(), selector));
-			System.err.println(String.format("Found %d Node Ids:", results.size())
-					+ ", explore first one");
+			List<NodeId> results = chromeDevTools.send(DOM.querySelectorAll(result.getNodeId(), selector));
+			System.err.println(String.format("Found %d Node Ids:", results.size()) + ", explore first one");
 
 			results.subList(0, 1).forEach(nodeId -> {
-				System.err.println(String.format("Id: %s\nHTML: %s", nodeId.toString(),
-						chromeDevTools.send(DOM.getOuterHTML(Optional.of(nodeId),
-								Optional.empty(), Optional.empty()))));
+				System.err.println(String.format("Id: %s\nHTML: %s", nodeId.toString(), chromeDevTools
+						.send(DOM.getOuterHTML(Optional.of(nodeId), Optional.empty(), Optional.empty()))));
 				// NOTE: WARNING: Unable to map result for DOM.getContentQuads
 				// org.openqa.selenium.json.JsonException:
 				// Unable to create instance of class
 				// org.openqa.selenium.devtools.v126.dom.model.Quad
 				// WARNING: Unable to process:
 				// {"id":9,"result":{"quads":[[360.5500183105469,661.1500244140625,578.3624877929688,661.1500244140625,578.3624877929688,747.1500244140625,360.5500183105469,747.1500244140625]]},"sessionId":"B816DDEDA9AEA89C97AF770D6885CD7E"}
-				// org.openqa.selenium.json.JsonException: Expected to read a END_MAP
+				// org.openqa.selenium.json.JsonException: Expected to read a
+				// END_MAP
 				// but instead have: END_COLLECTION. Last 128 characters read:
 				// 9,661.1500244140625,578.3624877929688,661.1500244140625,578.3624877929688,747.1500244140625,360.5500183105469,747.1500244140625]
-				// NOTE: typing he result2 to Object does not resolve
-				// java.lang.ClassCastException: java.util.ArrayList cannot be cast to
-				// org.openqa.selenium.devtools.v126.dom.model.Quad
-				// at
-				// org.openqa.selenium.devtools.v126.dom.model.Quad.fromJson(Quad.java:18)
-				List<Quad> result2 = chromeDevTools.send(DOM.getContentQuads(
-						Optional.of(nodeId), Optional.empty(), Optional.empty()));
-				System.err.println(String.format("result: " + result2));
+				List<Quad> result2 = chromeDevTools
+						.send(DOM.getContentQuads(Optional.of(nodeId), Optional.empty(), Optional.empty()));
+				System.err.println(String.format("result2: " + result2));
 
 				Quad result3 = result2.get(0);
+				// https://www.javadoc.io/doc/org.seleniumhq.selenium/selenium-devtools-v126/latest/org/openqa/selenium/devtools/v126/dom/model/package-summary.html
+				// An array of quad vertices, x immediately followed by y for
+				// each point, points clock-wise
+				System.err.println(String.format("result3: " + result3.toString()));
 
-				Long x = (Long) ((List<Long>) result3).get(0);
-				Long y = (Long) ((List<Long>) result3).get(1);
-				// System.err.println(String.format("quad: " + result2));
+				List<Double> data = (List<Double>) gson.fromJson(result3.toString(), List.class);
+				// java.lang.ClassCastException:
+				// class org.openqa.selenium.devtools.v126.dom.model.Quad
+				// cannot be cast to class java.util.List
+				Double x = data.get(0);
+				Double y = data.get(1);
 
-				chromeDevTools.send(
-						Input.dispatchMouseEvent(Input.DispatchMouseEventType.MOUSEPRESSED,
-								x, y, Optional.empty(), Optional.empty(), Optional.empty(),
-								Optional.of(1), Optional.of(1), Optional.empty(),
-								Optional.empty(), Optional.empty(), Optional.empty(),
-								Optional.empty(), Optional.empty(), Optional.empty(),
-								Optional.of(Input.DispatchMouseEventPointerType.MOUSE)));
+				chromeDevTools.send(Input.dispatchMouseEvent(Input.DispatchMouseEventType.MOUSEPRESSED, x, y,
+						Optional.empty(), Optional.empty(), Optional.empty(), Optional.of(1), Optional.of(1),
+						Optional.empty(), Optional.empty(), Optional.empty(), Optional.empty(), Optional.empty(),
+						Optional.empty(), Optional.empty(), Optional.of(Input.DispatchMouseEventPointerType.MOUSE)));
 				Utils.sleep(100);
-				chromeDevTools.send(
-						Input.dispatchMouseEvent(Input.DispatchMouseEventType.MOUSERELEASED,
-								x, y, Optional.empty(), Optional.empty(), Optional.empty(),
-								Optional.of(1), Optional.of(1), Optional.empty(),
-								Optional.empty(), Optional.empty(), Optional.empty(),
-								Optional.empty(), Optional.empty(), Optional.empty(),
-								Optional.of(Input.DispatchMouseEventPointerType.MOUSE)));
+				chromeDevTools.send(Input.dispatchMouseEvent(Input.DispatchMouseEventType.MOUSERELEASED, x, y,
+						Optional.empty(), Optional.empty(), Optional.empty(), Optional.of(1), Optional.of(1),
+						Optional.empty(), Optional.empty(), Optional.empty(), Optional.empty(), Optional.empty(),
+						Optional.empty(), Optional.empty(), Optional.of(Input.DispatchMouseEventPointerType.MOUSE)));
 			});
 
 		} catch (DevToolsException e) {
@@ -124,3 +121,4 @@ public class DOMElementClickDevToolsTest extends BaseDevToolsTest {
 		}
 	}
 }
+
